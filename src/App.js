76abbabcc61a9d2bed70ccfcff278f8ca9a730b6
nicholasrice <nicholas.rice119@gmail.com>
2019-09-09 21:54:24 -0700

    ie11-custom-prop-polyfill fail case

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            classNames: true
+        }
+    }
+    componentDidMount() {
+        import('ie11-custom-properties').then(() => {
+            console.log("imported")
+        });
+
+        window.setInterval(() => {
+            this.setState({classNames: !this.state.classNames})
+        }, 6000)
+    }
+    render() {
+        return (
+            <div className="App bar">
+                <header className={this.state.classNames ? "App-header foo" : "i"}>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
